88ea49ee82a7a9944d773e82b90fd3a6815b90f3
ionchiriac99 <i.chiriac1999@gmail.com>
2023-07-09 03:19:50 +0300

    added blank home page

--- src/app/core/routing.module.ts
+++ src/app/core/routing.module.ts
@@ -3,6 +3,10 @@ import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
 	{
+		path: '',
+		loadChildren: () => import('../pages/home/home.module').then((m) => m.HomeModule),
+	},
+	{
 		path: 'signin',
 		loadChildren: () => import('../pages/signin/signin.module').then((m) => m.SigninModule),
 	},
